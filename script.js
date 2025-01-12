e20d7cd94dc6622dfb86118464a5ab9d1849f798
loc856 <20001291@st.vlute.edu.vn>
2025-01-12 23:59:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Placeholder for JavaScript interactions (e.g., form validation, animations)
+// For now, this file is empty, but can be expanded later.
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Cảm ơn bạn đã gửi thông tin!');
+});
